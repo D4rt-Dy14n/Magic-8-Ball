0a6c02c1068b5eb7ae47017714317e29f4806910
D4rt-Dy14n <yuri.fedorov@inbox.ru>
2020-05-05 22:51:19 +0300

    Initial Commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         }
 
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        if motion == .motionShake {
+        AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+        imageView.image = ballArray.randomElement()
+}
 
 }
 
+}
